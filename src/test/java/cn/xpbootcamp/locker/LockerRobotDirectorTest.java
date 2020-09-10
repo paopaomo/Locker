bf256c9462fd4583f1840d3e279532d56d889654
Zi Ye <18612319597@163.com>
2020-09-10 21:54:39 +0800

    should_return_the_report_of_locker_use_when_query_given_1_manager_manage_2_robots_both_manage_2_lockers_and_not_manage_locker

--- src/test/java/cn/xpbootcamp/locker/LockerRobotDirectorTest.java
+++ src/test/java/cn/xpbootcamp/locker/LockerRobotDirectorTest.java
@@ -24,4 +24,31 @@ public class LockerRobotDirectorTest {
                 "\n\t\tL 4 5";
         Assert.assertEquals(expectReport, report.toString());
     }
+
+    @Test
+    public void should_return_the_report_of_locker_use_when_query_given_1_manager_manage_2_robots_both_manage_2_lockers_and_not_manage_locker() {
+        Locker locker1 = new Locker(1);
+        locker1.saveBag(new Bag());
+        Locker locker2 = new Locker(2);
+        locker2.saveBag(new Bag());
+        PrimaryLockerRobot robot1 = new PrimaryLockerRobot(List.of(locker1, locker2));
+        Locker locker3 = new Locker(3);
+        locker3.saveBag(new Bag());
+        Locker locker4 = new Locker(2);
+        locker4.saveBag(new Bag());
+        SmartLockerRobot robot2 = new SmartLockerRobot(List.of(locker3, locker4));
+        LockerRobotManager manager = new LockerRobotManager(List.of(robot1, robot2));
+        LockerRobotDirector director = new LockerRobotDirector(List.of(manager));
+
+        StringBuilder report = director.getReport();
+
+        String expectReport = "M 4 8" +
+                "\n\tR 1 3" +
+                "\n\t\tL 0 1" +
+                "\n\t\tL 1 2" +
+                "\n\tR 3 5" +
+                "\n\t\tL 2 3" +
+                "\n\t\tL 1 2";
+        Assert.assertEquals(expectReport, report.toString());
+    }
 }
